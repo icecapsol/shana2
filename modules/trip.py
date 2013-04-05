00f50f7a5f8a34739c104d20af7211238683dc53
Eli Patterson <icecapsol@gmail.com>
2013-04-04 22:07:31 -0400

    Fixed shift-jis handling

--- modules/trip.py
+++ modules/trip.py
@@ -12,7 +12,7 @@ def f_trip(phenny, input):
     salt = (pw + b'...')[1:3]
     salt = re.compile(b'[^\.-z]').sub(b'.', salt)
     salt = salt.translate(bytes.maketrans(b':;<=>?@[\\]^_`', b'ABCDEFGabcdef'))
-    trip = crypt.crypt(pw.decode("utf-8"), salt.decode("utf-8"))[-10:]
+    trip = crypt.crypt(pw.decode("shift_jis"), salt.decode("shift_jis"))[-10:]
     phenny.say("%s => %s" % (input.group(2), trip))
 
 f_trip.name = 'trip'
