9f766586d039b9b740d01b864122a2d1704f3edd
Eli Patterson <icecapsol@gmail.com>
2013-05-07 12:58:43 -0400

    Variable name changes phenny->shana input->event etc.

--- modules/un404.py
+++ modules/un404.py
@@ -1,25 +1,19 @@
 #!/usr/bin/env python
 from urllib.request import urlopen
-import re
 
-def f_un404_cache(phenny, input):
-	#httppattern = re.compile('(http[s]?:\/\/\S+)', re.M)
-	#urls = httppattern.findall(input.group(0))
-	
-	for url in input.searches:
+def un404_cache(shana, event):
+	for url in event.searches:
 		if url.find("images.4chan.org/") != -1:
 			cache = urlopen("http://gtrack.org/image_cache.php?mode=cache&url=%s" % url)
 			cache.close()
 
-f_un404_cache.name = 'un404'
-f_un404_cache.rule = (r'http[s]?:\/\/\S+')
-f_un404_cache.priority = 'low'
+un404_cache.name = 'un404_cache'
+un404_cache.rule = (r'http[s]?:\/\/\S+')
 
 
-def f_un404(phenny, input):
-	phenny.reply("http://gtrack.org/imgcache/%s" % input.group(2))
+def un404(shana, event):
+	shana.reply("http://gtrack.org/imgcache/%s" % event.group(2))
 	pass
 
-f_un404.name = 'un404'
-f_un404.commands = ['un404']
-f_un404.priority = 'low'
+un404.name = 'un404'
+un404.commands = ['un404']
