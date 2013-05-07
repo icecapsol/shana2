f504a4c23727c53cb97aca94936bcbdfed690b7f
Eli Patterson <icecapsol@gmail.com>
2013-05-07 14:50:32 -0400

    Variable name changes phenny->shana input->event etc.

--- modules/startup.py
+++ modules/startup.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python
-"""
-startup.py - Phenny Startup Module
-Copyright 2008, Sean B. Palmer, inamidst.com
-Licensed under the Eiffel Forum License 2.
-
-http://inamidst.com/phenny/
-"""
 import time
-def startup(phenny, input): 
-	if 'nickserv' in phenny.conf.keys():
-		phenny.write(('PRIVMSG', 'NickServ'), 'identify %s' % phenny.conf['nickserv'])
+def startup(shana, event): 
+	if 'nickserv' in shana.conf.keys():
+		shana.write(('PRIVMSG', 'NickServ'), 'identify %s' % shana.conf['nickserv'])
 		time.sleep(2)
 
-	for channel in phenny.conf['channels']: 
-		phenny.write(('JOIN', channel))
+	for channel in shana.conf['channels']: 
+		shana.write(('JOIN', channel))
 
 startup.rule = r'(.*)'
 startup.event = '251'
