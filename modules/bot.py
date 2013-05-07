c725d9027c565cf35553799711d0c9d7706b901c
Eli Patterson <icecapsol@gmail.com>
2013-05-07 15:49:30 -0400

    Variable name changes phenny->shana input->event etc.

--- modules/bot.py
+++ modules/bot.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python
 import threading, sys
 
-def store(phenny, input):
+def store(shana, event):
 	store = {}
 	while True:
-		l = phenny.recv()
+		l = shana.recv()
 		if l.subject == "STORE":
 			store[l.body['name']] = l.body['value']
 		elif l.subject == "SET DEFAULT":
 			if l.body['name'] in store.keys():
-				phenny.send(l.sender, "VARIABLE", {'name': l.body['name'], 'value': store[l.body['name']], 'error': None})
+				shana.send(l.sender, "VARIABLE", {'name': l.body['name'], 'value': store[l.body['name']], 'error': None})
 			else:
 				store[l.body['name']] = l.body['value']
-				phenny.send(l.sender, "VARIABLE", {'name': l.body['name'], 'value': store[l.body['name']], 'error': "Var set"})
+				shana.send(l.sender, "VARIABLE", {'name': l.body['name'], 'value': store[l.body['name']], 'error': "Var set"})
 		elif l.subject == "LOAD":
 			if l.body['name'] in store.keys():
-				phenny.send(l.sender, "VARIABLE", {'name': l.body['name'], 'value': store[l.body['name']], 'error': None})
+				shana.send(l.sender, "VARIABLE", {'name': l.body['name'], 'value': store[l.body['name']], 'error': None})
 			else:
-				phenny.send(l.sender, "VARIABLE", {'name': l.body['name'], 'value': None, 'error': "Not set"})
+				shana.send(l.sender, "VARIABLE", {'name': l.body['name'], 'value': None, 'error': "Not set"})
 
 store.name = 'store'
 store.wake_on_letter = True
 
-def logger(phenny, input):
+def logger(shana, event):
 	levels = {"FATAL": 0, "CRITICAL": 1, "ERROR": 2, "WARNING": 3, "CAUTION": 4, "NOTICE": 5, "STATUS": 6, "DEBUG": 7}
 	level = 6
 	while True:
-		l = phenny.inbox.recv()
+		l = shana.inbox.recv()
 		if l.subject == "LOG LEVEL":
 			if "level" in l.body:
 				level = levels[l.body['level']]
@@ -41,9 +41,13 @@ logger.name = 'logger'
 logger.service = True
 logger.wake_on_letter = True
 
-def reload_module(phenny, input):
-	print(input.sender)
-	phenny.send("self.launcher", "RELOAD MODULE", {'names': input.group(2).split(', '), 'sender': input.sender, 'nick': input.nick})
+def version(shana, event):
+	shana.say("Shana2 Version 0.1 Beta")
+version.commands = ['version']
+
+def reload_module(shana, event):
+	print(event.sender)
+	shana.send("self.launcher", "RELOAD MODULE", {'names': event.group(2).split(', '), 'sender': event.sender, 'nick': event.nick})
 	
 reload_module.name = 'reload_module'
 reload_module.commands = ['reload']
