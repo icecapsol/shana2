de1e6a9566b06cb4c9e7bf38c20438f21b138a59
Eli Patterson <icecapsol@gmail.com>
2013-05-07 14:49:10 -0400

    Variable name changes phenny->shana input->event etc.

--- modules/sting.py
+++ modules/sting.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 
-def f_sting(phenny, input):
-	phenny.say("*badum tiss*")
+def sting(shana, event):
+	shana.say("*badum tiss*")
 	
-f_sting.name = 'sting'
-f_sting.commands = ['sting']
-f_sting.priority = 'low'
+sting.name = 'sting'
+sting.commands = ['sting']
