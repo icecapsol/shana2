31819c68fd926265d69038d36cafd9109cf580e0
Eli Patterson <icecapsol@gmail.com>
2013-12-27 10:50:43 -0500

    New modules for looking up stuff.

---
+++ modules/isbn.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from bs4 import BeautifulSoup as BS
+from urllib.request import urlopen
+import re
+
+def isbn(shana, event):
+	url = "http://www.lookupbyisbn.com/Search/Book/%s/1"
+	numbers = re.compile('[0-9]')
+	raw = event.group(2)
+	isbnno = ''.join(numbers.findall(raw))
+	
+	if len(isbnno) not in [9, 10, 13]:
+		shana.reply("invalid ISBN")
+		return
+	if len(isbnno) == 9:
+		isbnno = "0"+isbnno
+	
+	try:
+		page = urlopen(url % isbnno)
+	except:
+		shana.say("ISBN not found")
+		return
+	soup = BS(page)
+	
+	for item in soup.find_all("li", style="margin-bottom:1em;"):
+		title = item.a.string
+		author = item.u.string
+		publisher = item.i.string
+		break
+	
+	shana.say("ISBN: %s => %s, %s, %s" % (isbnno, title, author, publisher))
+
+isbn.commands = ['isbn']
---
+++ modules/upc.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from bs4 import BeautifulSoup as BS
+from urllib.request import urlopen
+import re
+
+def upc(shana, event):
+	url = "http://www.upcdatabase.com/item/%s"
+	numbers = re.compile('[0-9]')
+	raw = event.group(2)
+	upcno = ''.join(numbers.findall(raw))
+	
+	if len(upcno) not in [8, 12, 13]:
+		shana.reply("invalid UPC")
+		return
+	
+	try:
+		page = urlopen(url % upcno)
+	except:
+		shana.say("UPC not found")
+		return
+	soup = BS(page)
+	
+	upca = "?"
+	ucc = "?"
+	desc = "?"
+	for item in soup.find_all("tr"):
+		if item.td.string == "UPC-A":
+			upca = item.find_all("td")[2].img['alt']
+		elif item.td.string == "EAN/UCC-13":
+			ucc = item.find_all("td")[2].img['alt']
+		elif item.td.string == "Description":
+			desc = item.find_all("td")[2].string
+	
+	shana.say("UPC-A: %s, EAN/UCC-13: %s => %s" % (upca, ucc, desc))
+
+upc.commands = ['upc']
