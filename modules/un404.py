38b1dd95f59ec5381758445ef0b95d57796deae9
btelle <brandon.telle@gmail.com>
2014-06-11 16:48:27 -0700

    Update un404.py
    Change 4chan image domain, use SSL on gtrack.org

--- modules/un404.py
+++ modules/un404.py
@@ -3,8 +3,8 @@ from urllib.request import urlopen
 
 def un404_cache(shana, event):
 	for url in event.searches:
-		if url.find("images.4chan.org/") != -1:
-			cache = urlopen("http://gtrack.org/image_cache.php?mode=cache&url=%s" % url)
+		if url.find("i.4cdn.org/") != -1:
+			cache = urlopen("https://gtrack.org/image_cache.php?mode=cache&url=%s" % url)
 			cache.close()
 
 un404_cache.name = 'un404_cache'
@@ -12,7 +12,7 @@ un404_cache.rule = (r'http[s]?:\/\/\S+')
 
 
 def un404(shana, event):
-	shana.reply("http://gtrack.org/imgcache/%s" % event.group(2))
+	shana.reply("https://gtrack.org/imgcache/%s" % event.group(2))
 	pass
 
 un404.name = 'un404'
